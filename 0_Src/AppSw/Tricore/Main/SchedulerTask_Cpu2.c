8e9353e1131f662da48ff8371e842088e85ec15c
tkdgusqkr <sanghyean1@hanyang.ac.kr>
2025-03-19 20:52:09 +0900

    feat: fix

--- 0_Src/AppSw/Tricore/Main/SchedulerTask_Cpu2.c
+++ 0_Src/AppSw/Tricore/Main/SchedulerTask_Cpu2.c
@@ -117,8 +117,6 @@ void Task_core2_1ms(void)
 	
 	if(task2_10ms_counter == 10)
 	{
-		CanCommunication_reInit();
-
 		AmkInverter_writeMessageFront(torque_fl, torque_fr, accelerating);
 		AmkInverter_writeMessageRear(torque_rl, torque_rr, accelerating);
 	}
